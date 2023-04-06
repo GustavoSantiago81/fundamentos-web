c435e9ce7d6e92bdd5ea084e8fea7313752467d8
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-04-06 14:44:38 -0300

    style no js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-var
-let
-const
+
+/* 
+Case Sensitive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nomeg: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()                   melhor prática
+*/
+
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
